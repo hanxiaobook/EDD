5b0c2b2cb9e98b5f9f2907c82591282a5482bf01
hanxiaobook <hanxiaobook@outlook.com>
2019-09-27 15:50:34 +0800

    home页面逻辑

--- dist/pages/home/index.js
+++ dist/pages/home/index.js
@@ -1,15 +1,29 @@
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
-  value: true
+    value: true
 });
 exports.default = Page({
-  data: {
-    isShow: false
-  },
-  starShow: function starShow() {
-    this.setData({
-      isShow: true
-    });
-  }
+    data: {
+        isShow: false,
+        imageWidth: 0
+    },
+    onLoad: function onLoad() {
+        this.setData({
+            imageWidth: wx.getSystemInfoSync().windowWidth
+        });
+        console.log(this.data.imageWidth);
+    },
+    chickCamera: function chickCamera() {
+        wx.chooseImage({
+            count: 1,
+            sizeType: ['original'],
+            sourceType: ['camera'],
+            success: function success(res) {
+                // tempFilePath可以作为img标签的src属性显示图片
+                var tempFilePaths = res.tempFilePaths;
+                //TODO:发送图片路径到服务器
+            }
+        });
+    }
 });
